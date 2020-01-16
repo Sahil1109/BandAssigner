805e2ef0e2c03bbc1dfdf6840cf37d76f69482bb
CO16348Sahil <35717032+CO16348Sahil@users.noreply.github.com>
2020-01-16 11:13:49 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    let bpm= 100;
+    let playing=false;
+    return (
+      <div className="App">
+        <div className="slider">
+        <div>{bpm} BPM</div>
+        <input type="range" min="60" max="240" value={bpm}></input>
+        </div>
+        <button>{playing? 'Start':'Stop'} </button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
