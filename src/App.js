aad9abf58d3f385c653d40598c65049c28623f54
CO16348Sahil <35717032+CO16348Sahil@users.noreply.github.com>
2020-01-17 12:38:21 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,156 +1,47 @@
+import React, {Component} from 'react'
+import Myheader from './Components/Myheader';
+import Profiles from './Components/Profiles';
+import Searchform from './Components/SearchForm';
 import './App.css'
-import React from 'react'
-
-
-
-function App() {
-  return (
-    <div>
-    
-    </div>
-  )
-}
-
-export default App
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{
-/* import click1 from './click1.wav'
-import click2 from './click2.wav'
 
 class App extends Component {
+
   constructor(props) {
     super(props)
   
     this.state = {
-       bpm:100,
-       playing: false,
-       beatsPerMeasure:3,
-       count:0,
-
-    }
-    //
-    this.click1= new Audio(click1);
-    this.click2=new Audio(click2);
-  }
-  handleBpmChange = event =>{
-    const bpm = event.target.value;
-
-    if (this.state.playing) {
-      // Stop the old timer and start a new one
-      clearInterval(this.timer);
-
-      this.timer = setInterval(this.playClick, (60 / bpm) * 1000);
-  
-      // Set the new BPM, and reset the beat counter
-      this.setState({
-        count: 0,
-        bpm
-      });
-    }
-     else {
-      // Otherwise just update the BPM
-      this.setState({ bpm });
+       searchText: 'Sahil1109',
+       data: '',
     }
   }
-  playClick = () => {
-    const { count, beatsPerMeasure } = this.state;
-  
-    // The first beat will have a different sound than the others
-    if (count % beatsPerMeasure === 0) {
-      this.click2.play();
-    } else {
-      this.click1.play();
-    }
-  
-    // Keep track of which beat we're on
-    this.setState(state => ({
-      count: (state.count + 1) % state.beatsPerMeasure
-    }));
-  };
-
-  startStop=()=>{
-    if (this.state.playing) {
-      // Stop the timer
-      clearInterval(this.timer);
+  fetchSearch=username=>{
+    const API = 'https://api.github.com/';
+    let url=`${API}search/users?q=${username}`;
+    fetch(url)
+    .then((res)=>res.json())
+    .then((data)=>{
       this.setState({
-        playing: false
+        data: data
       });
-    } else {
-      // Start a timer with the current BPM
-      this.timer = setInterval(
-        this.playClick,
-        (60 / this.state.bpm) * 1000
-      );
-      this.setState(
-        {
-          count: 0,
-          playing: true
-          // Play a click "immediately" (after setState finishes)
-        },
-        this.playClick
-      );
-    }
-  };
+    })
+    .catch((error)=>
+    console.log('Oops! There is a problem'+error))
+  }
+  componentDidMount(){
+    
+    this.fetchSearch(this.state.searchText);
+  }
   
   render() {
-    const {bpm, playing}=this.state
+    
     return (
-      <div className="App">
-        <div className="slider">
-        <div>{bpm} BPM</div>
-        <input 
-        type="range" 
-        min="60" 
-        max="240" 
-        value={bpm} 
-        onChange={this.handleBpmChange}>
-        </input>
-        </div>
-        <button onClick={this.startStop}>{playing? 'Stop':'Start'} </button>
+      <div>
+        <Myheader/>
+        <Searchform  fetchSearch={this.fetchSearch}/>
+        <Profiles data={this.state.data}/>
       </div>
     )
   }
-}  */}
+}
 
-
+export default App
